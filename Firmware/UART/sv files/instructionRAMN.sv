5daeb354d3b6fe701142a649d6aad5a5b63fc481
yismailsaadelgedawy <youssefgedd@gmail.com>
2024-02-16 23:54:19 +0000

    [UNTESTED] instruction RAM v1 completed
    still need to add a CPU random access fetch mode later

---
+++ Firmware/UART/sv files/instructionRAMN.sv
@@ -0,0 +1,164 @@
+module instructionRAMN #(parameter N=8, MAX_ADDRESS=255) (
+
+    output logic [N-1:0] data_out,
+
+    input logic [N-1:0] data_in,
+    input logic [1:0] MODE,
+    input logic [N-1:0] address,
+    input logic clk, rst, DEBUG
+
+);
+
+
+// MODE may have 3 values
+// 0 : read from RAM in FIFO order
+// 1 : write to RAM in FIFO order
+// 2 : debug read from RAM in FIFO order
+// 3 : CPU fetch via random access (later)
+
+
+// address registers that will be used depending on the MODE input
+logic [N-1:0] addressREAD_reg = 'd0;
+logic [N-1:0] addressWRITE_reg = 'd0;
+logic [N-1:0] addressDEBUG_reg = 'd0;
+
+
+
+// RAM creation
+// MAX_ADDRESS * N-bit memory locations
+logic [N-1:0] ram [MAX_ADDRESS:0];
+
+
+// loop var
+int unsigned i=0;
+
+
+// registers used for rising edge detection
+// of DEBUG input
+logic current_reg = 0;
+logic prev_reg = 0;
+
+// this clock controls how fast the read operation is (MODE 0)
+always_ff @(posedge clk) begin
+
+    if(rst) begin
+
+        addressREAD_reg = 'd0;
+        addressWRITE_reg = 'd0;
+        addressDEBUG_reg = 'd0;
+        data_out <= 'd0;
+
+        // clearing out all RAM contents
+        for (i=0; i<MAX_ADDRESS+1; i++) begin
+
+            ram[i] <= 'd0;
+
+        end
+
+
+    end
+
+    else begin
+
+        case(MODE)
+
+        
+        // READ
+        2'd0 : begin
+
+            // clear other address registers upon switching modes
+            //addressREAD_reg = 'd0;
+            addressWRITE_reg <= 'd0;
+            addressDEBUG_reg <= 'd0;
+
+            data_out <= ram[addressREAD_reg];
+
+            // checks that
+            // a) not at last memory location and,
+            // b) next mem location not empty (not containing zero)
+            if (addressREAD_reg != MAX_ADDRESS && ram[addressREAD_reg + 1] != 'd0) begin
+
+                addressREAD_reg <= addressREAD_reg + 1;
+
+            end
+
+        end
+
+        // WRITE
+        // will detect the character '$' 0x24
+        // to indicate next byte
+        2'd1 : begin
+
+            // clear other address registers upon switching modes
+            addressREAD_reg <= 'd0;
+            //addressWRITE_reg = 'd0;
+            addressDEBUG_reg <= 'd0;
+
+            // ASCII '$' byte
+            if (data_in != 'h24) begin
+
+                ram[addressWRITE_reg] <= data_in;
+
+            end
+            
+            else begin
+
+                addressWRITE_reg <= addressWRITE_reg + 1;
+
+            end
+
+        end
+
+        // DEBUG READ
+        2'd2 : begin
+
+            // clear other address registers upon switching modes
+            addressREAD_reg <= 'd0;
+            addressWRITE_reg <= 'd0;
+            //addressDEBUG_reg = 'd0;
+
+            current_reg <= DEBUG; // clk cycle 1
+            prev_reg <= current_reg; // clk cycle 2
+
+            data_out <= ram[addressDEBUG_reg];
+
+
+            // rising edge detector
+            if (current_reg && !prev_reg) begin
+
+                if (addressDEBUG_reg != MAX_ADDRESS && ram[addressDEBUG_reg + 1] != 'd0) begin
+
+                    addressDEBUG_reg <= addressDEBUG_reg + 1;
+
+                end
+
+
+            end
+
+            
+
+        end
+
+        // default case is WRITE
+        // default : begin
+
+
+        // end
+
+
+
+        endcase
+
+
+
+    end
+
+
+
+
+end
+
+
+
+
+endmodule
---
+++ Firmware/UART/sv files/instructionRAMN_tb.sv
@@ -0,0 +1,218 @@
+module instructionRAMN_tb;
+
+// parameters
+parameter N=8, MAX_ADDRESS=255;
+
+
+// internal wires
+
+// in
+logic clk, rst, DEBUG;
+logic [1:0] MODE;
+logic [N-1:0] address;
+logic [N-1:0] data_in;
+
+
+// out
+logic [N-1:0] data_out;
+
+
+// wiring
+instructionRAMN dut (
+
+    // in
+    .clk(clk),
+    .rst(rst),
+    .DEBUG(DEBUG),
+    .MODE(MODE),
+    .address(address),
+    .data_in(data_in),
+
+    // out
+    .data_out(data_out)
+
+
+);
+
+
+// 50MHz clock
+// T = 20ns
+always begin
+
+    clk = 1;
+    #10ns;
+    clk = 0;
+    #10ns;
+
+end
+
+// testing -- writing MUST be done first
+initial begin
+
+    // reset state
+    rst = 1;
+    MODE = 2'd1; // write mode by default
+    DEBUG = 0; // debug button not pressed
+    address = 'd0;
+    data_in = 'd0;
+
+    #15ns;
+    assert (data_out == 'd0) $display("passed reset"); else $error("failed reset");
+    #5ns;
+    
+    rst = 0;
+
+    $display(" ");
+    $display("////////////// WRITING TO RAM //////////////");
+    $display(" ");
+
+    // data_out should stay 0
+
+    MODE = 2'd1; // write mode by default
+    DEBUG = 0; // ignored
+    address = 'd0; // ignored
+    data_in = 'h4A;
+    #20ns;
+
+    // next address byte
+    data_in = 'h24;
+    #20ns;
+
+    data_in = 'h4B;
+    #20ns;
+
+    data_in = 'h24;
+    #20ns;
+
+    data_in = 'h4C;
+    #20ns;
+
+    data_in = 'h24;
+    #20ns;
+
+    data_in = 'h4D;
+    #20ns;
+
+    data_in = 'h24;
+    #20ns;
+
+
+
+    $display(" ");
+    $display("////////////// READING FROM RAM //////////////");
+    $display(" ");
+
+    MODE = 2'd0; // read mode
+    DEBUG = 0; // ignored
+    address = 'd0; // ignored
+    data_in = 'd0; // ignored
+
+    #15ns;
+    assert (data_out == 'h4A) $display("passed reading 1..."); else $error("failed reading 1...");
+    #5ns;
+
+    #15ns;
+    assert (data_out == 'h4B) $display("passed reading 2..."); else $error("failed reading 2...");
+    #5ns;
+
+    #15ns;
+    assert (data_out == 'h4C) $display("passed reading 3..."); else $error("failed reading 3...");
+    #5ns;
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed reading 4..."); else $error("failed reading 4...");
+    #5ns;
+
+
+
+    #60ns;  // run for a bit
+
+
+
+
+    $display(" ");
+    $display("////////////// DEBUGGING FROM RAM //////////////");
+    $display(" ");
+
+    // address and DEBUG should be ignored
+    MODE = 2'd2; // debug mode
+    DEBUG = 0;
+    address = 'd0; // ignored
+    data_in = 'd0; // ignored
+
+    #15ns;
+    assert (data_out == 'h4A) $display("passed debug 1..."); else $error("failed debug 1...");
+    #5ns;
+
+    DEBUG = 1; #20ns; // button press
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4B) $display("passed debug 2..."); else $error("failed debug 2...");
+    #5ns;
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4C) $display("passed debug 3..."); else $error("failed debug 3...");
+    #5ns;
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed debug 4..."); else $error("failed debug 4..");
+    #5ns;
+
+
+
+    ////// pressing btn few more times to stress test //////
+
+    $display(" ");
+    $display("////// pressing btn few more times to stress test //////");
+    $display(" ");
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed debug: end of instructions"); else $error("failed passed debug: end of instructions");
+    #5ns;
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed debug: end of instructions"); else $error("failed passed debug: end of instructions");
+    #5ns;
+
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed debug: end of instructions"); else $error("failed passed debug: end of instructions");
+    #5ns;
+
+    DEBUG = 1; #20ns; // button press, wait a clock cycle
+    DEBUG = 0;
+    #20ns; // wait a cycle
+
+    #15ns;
+    assert (data_out == 'h4D) $display("passed debug: end of instructions"); else $error("failed passed debug: end of instructions");
+    #5ns;
+
+
+
+    $stop;
+
+end
+
+endmodule
